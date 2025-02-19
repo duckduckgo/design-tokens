e2b1fa3e3c330fb0ba30083a7be158521706bd10
Marcos Gurgel <mgurgel@duckduckgo.com>
2025-02-19 11:45:39 -0500

    Bringing back dist

--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -17,7 +17,7 @@ const defaultOptions = {
 };
 
 export default {
-    source: ['src/properties/**/*.{js,json}'],
+    source: ['dist/properties/**/*.{js,json}'],
     log: {
         warnings: logWarningLevels.warn, // 'warn' | 'error' | 'disabled'
         verbosity: logVerbosityLevels.default, // 'default' | 'silent' | 'verbose'
