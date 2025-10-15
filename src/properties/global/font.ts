67550dc417dd760b421628dd7cc6bc3ada221c42
Emilia Ponomareva <eponomareva@duckduckgo.com>
2025-10-15 22:18:59 +0200

    add font sizes (#5)

---
+++ src/properties/global/font.ts
@@ -0,0 +1,28 @@
+import { DesignToken } from 'style-dictionary/types';
+
+const font: DesignToken = {
+    $type: 'dimension',
+    undo: 'none',
+    display: {
+        fontSize: {
+            title: {
+                '01': { $value: '64px' },
+                '02': { $value: '48px' },
+                '03': { $value: '36px' },
+                '04': { $value: '24px' },
+                '05': { $value: '20px' },
+                '06': { $value: '16px' },
+            },
+            body: {
+                '01': { $value: '32px' },
+                '02': { $value: '24px' },
+                '03': { $value: '20px' },
+                '04': { $value: '16px' },
+                '05': { $value: '14px' },
+                '06': { $value: '12px' },
+            },
+        },
+    },
+};
+
+export default font;
