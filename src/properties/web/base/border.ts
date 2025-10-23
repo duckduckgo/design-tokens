f76183c8650d1e987e588095e188c5d98be7ccb1
Emilia Ponomareva <eponomareva@duckduckgo.com>
2025-10-23 15:53:49 +0200

    update border radius name to radius (#7)

--- src/properties/web/base/border.ts
+++ src/properties/web/base/border.ts
@@ -3,36 +3,36 @@ import { DesignToken } from 'style-dictionary/types';
 const border: DesignToken = {
     $type: 'dimension',
     undo: 'none',
-    border: {
-        radius: {
-            x01: {
-                $value: '4px',
-            },
-            x02: {
-                $value: '8px',
-            },
-            x03: {
-                $value: '12px',
-            },
-            x04: {
-                $value: '16px',
-            },
-            x05: {
-                $value: '20px',
-            },
-            x06: {
-                $value: '24px',
-            },
-            x07: {
-                $value: '28px',
-            },
-            x08: {
-                $value: '32px',
-            },
-            full: {
-                $value: '999px',
-            },
+    radius: {
+        x01: {
+            $value: '4px',
         },
+        x02: {
+            $value: '8px',
+        },
+        x03: {
+            $value: '12px',
+        },
+        x04: {
+            $value: '16px',
+        },
+        x05: {
+            $value: '20px',
+        },
+        x06: {
+            $value: '24px',
+        },
+        x07: {
+            $value: '28px',
+        },
+        x08: {
+            $value: '32px',
+        },
+        full: {
+            $value: '999px',
+        },
+    },
+    border: {
         width: {
             base: {
                 $value: '1px',
