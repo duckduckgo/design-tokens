5599b1db3a688fc3bc17afba72a73ffe45976dff
Emilia Ponomareva <eponomareva@duckduckgo.com>
2025-11-04 07:43:34 -0500

    add z index tokens

--- src/properties/web/base/zindex.ts
+++ src/properties/web/base/zindex.ts
@@ -4,14 +4,13 @@ const Zindexes: DesignToken = {
     $type: 'dimension',
     undo: 'none',
     zIndex: {
-        deepdive: { $value: -99999 },
+        'page-background': { $value: -1 },
         default: { $value: 1 },
-        sticky: { $value: 1000 },
-        dropdown: { $value: 1100 },
-        'dialog-backdrop': { $value: 1200 },
-        dialog: { $value: 1300 },
-        notification: { $value: 1600 },
-        alert: { $value: 1700 },
+        popover: { $value: 100 },
+        'side-menu': { $value: 200 },
+        'modal-backdrop': { $value: 300 },
+        modal: { $value: 305 },
+        alert: { $value: 400 },
     },
 };
 
