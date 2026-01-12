35ffeec125a6fc85c0ddf01547b69cf77f2b8b0d
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2026-01-12 14:01:51 -0700

    feat: Add font and typography values

---
+++ src/properties/web/base/font.ts
@@ -0,0 +1,47 @@
+import { DesignToken } from 'style-dictionary/types';
+
+// Definitions for DuckSans + rebrand, these values will be used by the typography properties
+const font: DesignToken = {
+    $type: 'font',
+    font: {
+        family: {
+            // For usage on text size 19px or larger
+            display: {
+                $value: "DuckSansDisplay, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif",
+                comment: 'Use for titles and any text over 19px or more in font-size',
+            },
+            // For usage on text size 18px or smaller
+            product: {
+                $value: "'DuckSansProduct', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif",
+                comment: 'Use for most body text, any usage 18px or smaller in font-size',
+            },
+            monospace: {
+                $value: "ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono', 'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro','Fira Mono', 'Droid Sans Mono', 'Courier New', monospace",
+            },
+        },
+        weight: {
+            normal: {
+                $value: '400',
+            },
+            semibold: {
+                $value: '500',
+            },
+            bold: {
+                $value: '700',
+            },
+        },
+        letterSpacing: {
+            narrow: {
+                $value: '-1',
+            },
+            normal: {
+                $value: '0',
+            },
+            wide: {
+                $value: '2',
+            },
+        },
+    },
+};
+
+export default font;
