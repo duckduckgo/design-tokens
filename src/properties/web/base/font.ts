51ef9e2fc9a69925e1f3d3e8174c5f60865fe676
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2026-01-14 13:08:26 -0700

    feat: Add starter colors, sizes for staticpages

--- src/properties/web/base/font.ts
+++ src/properties/web/base/font.ts
@@ -7,7 +7,7 @@ const font: DesignToken = {
         family: {
             // For usage on text size 19px or larger
             display: {
-                $value: "DuckSansDisplay, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif",
+                $value: "'DuckSansDisplay', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif",
                 comment: 'Use for titles and any text over 19px or more in font-size',
             },
             // For usage on text size 18px or smaller
@@ -16,7 +16,7 @@ const font: DesignToken = {
                 comment: 'Use for most body text, any usage 18px or smaller in font-size',
             },
             monospace: {
-                $value: "ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono', 'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro','Fira Mono', 'Droid Sans Mono', 'Courier New', monospace",
+                $value: "ui-monospace, Menlo, Monaco, 'Cascadia Mono', 'Segoe UI Mono', 'Roboto Mono', 'Oxygen Mono', 'Ubuntu Monospace', 'Source Code Pro', 'Fira Mono', 'Droid Sans Mono', 'Courier New', monospace",
             },
         },
         weight: {
