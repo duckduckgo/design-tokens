ef72572840841142a29ffbf6cd55614ec1b60d9e
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2026-01-14 13:30:20 -0700

    wip: motif colors

---
+++ src/properties/web/base/motif-colors.ts
@@ -0,0 +1,94 @@
+import { DesignToken } from 'style-dictionary/types';
+
+const motifColors: DesignToken = {
+    $type: 'color',
+    motif: {
+        blossom: {
+            primary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            secondary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            tertiary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+        },
+        lilypad: {
+            primary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            secondary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            tertiary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+        },
+        mandarin: {
+            primary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            secondary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            tertiary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+        },
+        pollen: {
+            primary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            secondary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            tertiary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+        },
+        pondwater: {
+            primary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            secondary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+            tertiary: {
+                surfaceBackgroundColor: { $value: '#000000' },
+                textColor: { $value: '#000000' },
+                decorationColor: { $value: '#000000' },
+            },
+        },
+    },
+};
+
+export default motifColors;
