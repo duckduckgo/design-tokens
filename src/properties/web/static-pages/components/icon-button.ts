0e4556fd5ffd5eb1b94f817f81753084809dc8ba
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2026-01-26 18:44:48 -0700

    feat: Add Icon Button values, and some test tab values to try out in app

---
+++ src/properties/web/static-pages/components/icon-button.ts
@@ -0,0 +1,61 @@
+import { DesignToken } from 'style-dictionary/types';
+
+const iconButton: DesignToken = {
+    iconButton: {
+        // Border radius is the same for all sizes
+        borderRadius: {
+            $value: '{radius.full}',
+        },
+        xxs: {
+            // Padding is the same for all sizes
+            padding: {
+                $value: '{space.x1_5}',
+            },
+            // Icon size is for height and width values
+            iconSize: {
+                $value: '16px',
+            },
+        },
+        xs: {
+            padding: {
+                $value: '{space.x02}',
+            },
+            iconSize: {
+                $value: '16px',
+            },
+        },
+        sm: {
+            padding: {
+                $value: '10px',
+            },
+            iconSize: {
+                $value: '16px',
+            },
+        },
+        md: {
+            padding: {
+                $value: '{space.x03}',
+            },
+            iconSize: {
+                $value: '16px',
+            },
+        },
+        lg: {
+            padding: {
+                $value: '{space.x04}',
+            },
+            iconSize: {
+                $value: '16px',
+            },
+        },
+        xl: {
+            padding: {
+                $value: '{space.x04}',
+            },
+            iconSize: {
+                $value: '24px',
+            },
+        },
+    },
+};
+export default iconButton;
