f8d369c68b395b86aff313b97d3705e76cf47040
Valerie Kraucunas <vkraucunas@duckduckgo.com>
2026-02-26 21:48:59 -0700

    chore: Rename some formatters, update theme-colors outputs, create base files for sp instead of one tokens sheet to rule them all

---
+++ src/formats/sp-base-tokens.js
@@ -0,0 +1,40 @@
+import { fileHeader, formattedVariables } from 'style-dictionary/utils';
+
+function extractVariableLines(cssVariables) {
+    return cssVariables
+        .split('\n')
+        .filter((line) => line.trim() !== '' && line.trim() !== ':root {' && line.trim() !== '}')
+        .join('\n');
+}
+
+export default async function spBaseTokens({ dictionary, file, options }) {
+    const header = await fileHeader({ file });
+    const { sections = [] } = options;
+    const showSectionComments = sections.length > 1;
+
+    const sectionOutputs = sections
+        .map((section) => {
+            const tokens = dictionary.allTokens.filter((token) => token.path[0] === section.pathPrefix);
+
+            if (tokens.length === 0) {
+                return '';
+            }
+
+            const filteredDictionary = { ...dictionary, allTokens: tokens };
+            const cssVariables = formattedVariables({
+                format: 'css',
+                dictionary: filteredDictionary,
+                ...options,
+            });
+            const variableLines = extractVariableLines(cssVariables);
+
+            if (showSectionComments) {
+                return `  /* *** ${section.comment} *** */\n${variableLines}`;
+            }
+
+            return variableLines;
+        })
+        .filter((output) => output !== '');
+
+    return header + '\n:root {\n' + sectionOutputs.join('\n\n') + '\n}\n';
+}
