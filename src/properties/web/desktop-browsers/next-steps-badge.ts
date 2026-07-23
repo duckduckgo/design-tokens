53c466fc4c49714b08b5be92c2b5777ac9f0b4b7
Matteo Borgato <mborgato@duckduckgo.com>
2026-07-23 18:47:13 -0300

    feat: Add next-steps-card dismiss button semantic tokens

--- src/properties/web/desktop-browsers/next-steps-badge.ts
+++ src/properties/web/desktop-browsers/next-steps-badge.ts
@@ -6,6 +6,12 @@ const nextStepsBadgeTokens: DesignToken = {
         background: { $value: '{color.pollen.30}' },
         color: { $value: '{color.pollen.90}' },
     },
+    'next-steps-card': {
+        'dismiss-btn': {
+            border: { $value: '{color.blackAt.9}' },
+            color: { $value: '{color.blackAt.60}' },
+        },
+    },
 };
 
 export default nextStepsBadgeTokens;
